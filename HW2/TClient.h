ac4f5bd0d3ca327d183af2bef2c5080c9b767194
fdiaz3000 <fdiaz3000@gmail.com>
2017-07-28 20:20:34 -0400

    remove all files

--- HW2/TClient.h
+++
@@ -1,7 +0,0 @@
-#ifndef _client_
-#define _client_
-void setCommands(char** commands, char* buffer, int BUFFER_SIZE);
-void serverProcess(int sockfd, char* buffer, int BUFFER_SIZE, char** commands);
-void chatMode(int sockfd, char* buffer, int BUFFER_SIZE, char** commands);
-void* readMSG(void* arg);
-#endif
